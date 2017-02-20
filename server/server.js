49adcd5c28e230183e0627d77836c5ee5ffcf0d6
[jsw324] <jwalkow@tallience.com>
2017-02-20 13:51:07 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,15 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'jason',
-    text: 'what up',
-    createdAt: 2343
-  });
-
   socket.on('createMessage', (message) => {
     console.log('got message', message);
-  })
+    io.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+    });
+  });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
